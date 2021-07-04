47f1bd401a333f9779bd77473375a4430be73bf2
andiahmads <andi.fivesco@gmail.com>
2021-07-04 21:05:53 +0700

    complate reverse string using stack

--- struktur_data/ReverseStringUsingStack.java
+++ struktur_data/ReverseStringUsingStack.java
@@ -1,6 +1,37 @@
 package com.company.algoritm_problem_solving.struktur_data;
 
+import java.util.Scanner;
+
 public class ReverseStringUsingStack {
 
-    //next day
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("masukkan kata kata: ");
+        String input = sc.nextLine();
+        Stack myStack = new Stack(1000);
+
+
+        //BUAT TEMPORARY VARIABEL
+        int i = 0;
+
+        //memecah string menajadi character
+        for (char c : input.toCharArray()) {
+            //lakukan push
+            myStack.push("" + c);
+            //c berisi string yang sudah dipecah menjadi character.
+//            System.out.println(c);
+
+            //misalkan kata yang diinput tersebut adalah itu
+            // [i,t,u] -> convert ke stack, hasilnya [u,t,i]
+            //karna element pada pada character lah yang menjadi index 0 pada sebuah stack
+        }
+        System.out.println("hasil reverse: ");
+
+        while (!myStack.isEmpty()) {
+            System.out.print(myStack.pop());
+        }
+        System.out.println();
+
+    }
 }
