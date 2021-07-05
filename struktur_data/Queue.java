2eb15e5078de7bf862599737039a15809d416434
andiahmads <andi.fivesco@gmail.com>
2021-07-06 06:05:04 +0700

    Penjelasan tentang queue

---
+++ struktur_data/Queue.java
@@ -0,0 +1,22 @@
+package com.company.algoritm_problem_solving.struktur_data;
+
+public class Queue {
+
+    APA ITU QUEUE(ANTRIAN)
+    queue dapat dianalogikan seperti antrian loket, dimana orang yang pertama masuk antrian
+    akan menjadi orang yang pertama dilayani.
+    perbedaan: stack memiliki 1 pintu, sedangkan queue memiliki 2 pintu
+
+    Karakteristik queue adalah sistematika input dan output elementnya menggunakan sistematika FIFO(first in first out)
+    data yang pertamakali dimasukkan kedalam queue adalah data yang pertama kali keluar
+
+    Penggunaan queue
+    1. menyimpan langkah penelusuran graph menggunakan algoritm BFS, flood fill, brute force
+
+    Method2 pada queue
+    1.add() = push data kedalam queue
+    2.poll() = mengambil & membaca data dari queue
+    3.peek() = membaca data
+    4.getSize() = melihat size dari queue
+
+}
